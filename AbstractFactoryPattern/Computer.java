cfe69fbe1017f47547ad65ec6f36f328aa73edf0
chenshuang365 <54885661+chenshuang365@users.noreply.github.com>
2020-04-25 12:00:20 +0800

    删除AbstractFactoryPattern

--- AbstractFactoryPattern/Computer.java
+++
@@ -1,9 +0,0 @@
-package AbstractFactoryPattern;
-
-public abstract class Computer {
-	/**
-	 * 定义一个电脑的抽象类和一个抽象方法实现这个类
-	 */
-public abstract void start();
-
-}
--- AbstractFactoryPattern/LenovoComputer.java
+++
@@ -1,8 +0,0 @@
-package AbstractFactoryPattern;
-
-public class LenovoComputer extends Computer{
-	@Override
-	public void start() {
-		System.out.println("生产联想电脑");//定义一个具体的产品类
-	}
-}
